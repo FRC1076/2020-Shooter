33909633b05517151c69bef480828e4f918c940d
William Ballbach <2021ballbachwilliamd@aaps.k12.mi.us>
2020-01-31 18:57:09 -0500

    Press A to go full speed

--- robot.py
+++ robot.py
@@ -8,8 +8,8 @@ import logging
 MOTOR1 = 0
 MOTOR2 = 1
 
-# Encoder Constants
-# made up values, must do testing
+#Encoder Constants
+#made up values, must do testing
 DISTANCE_PER_PULSE = 1.0 
 MAX_PERIOD = 1.0
 MIN_RATE = 1.0
@@ -26,9 +26,9 @@ class Robot(wpilib.TimedRobot):
         # self.encoder = wpilib.Encoder(0,1)
         
         #display motor rpm
-        NetworkTables.initialize()
-        logging.basicConfig(level = logging.DEBUG)
-        self.sd = NetworkTables.getTable("SmartDashboard")
+        # NetworkTables.initialize()
+        # logging.basicConfig(level = logging.DEBUG)
+        # self.sd = NetworkTables.getTable("SmartDashboard")
 
     def robotPeriodic(self):
         return
@@ -36,7 +36,7 @@ class Robot(wpilib.TimedRobot):
     def teleopInit(self):
         print("TELEOP BEGINS")
 
-        # Encoder parameters initialization.
+        #Encoder parameters initialization.
         # self.encoder.reset()
         # self.encoder.setDistancePerPulse(DISTANCE_PER_PULSE)
         # self.encoder.setMaxPeriod(MAX_PERIOD)
@@ -45,12 +45,16 @@ class Robot(wpilib.TimedRobot):
 
         # self.motor2 = ctre.WPI_TalonSRX(PORT)
         # self.drive = DifferentialDrive(self.motor1, self.motor2)
-        # setup wheel diameter
+        #setup wheel diameter
 
     def teleopPeriodic(self):
         forward = self.stick.getRawAxis(5)
+        while self.stick.getAButtonPressed() == True:
+            forward = 1
+        
         self.motor1.set(forward)
         self.motor2.set(-forward)
+
         # print(self.encoder.get())
         # QuadPosition = self.motor1.getQuadraturePosition()
         # print(QuadPosition)
