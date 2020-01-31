f06b4f2ddba478bf930362c5fbb3c80d61b4cef4
William Ballbach <2021ballbachwilliamd@aaps.k12.mi.us>
2020-01-31 18:27:18 -0500

    Now does nothing at all but run shooter

--- robot.py
+++ robot.py
@@ -5,8 +5,8 @@ import robotpy_ext.common_drivers
 from networktables import NetworkTables
 import logging
 
-MOTOR1 = 1
-MOTOR2 = 2
+MOTOR1 = 0
+MOTOR2 = 1
 
 # Encoder Constants
 # made up values, must do testing
@@ -23,7 +23,7 @@ class Robot(wpilib.TimedRobot):
         self.stick = wpilib.XboxController(0)
         self.motor1 = ctre.WPI_TalonSRX(MOTOR1)
         self.motor2 = ctre.WPI_TalonSRX(MOTOR2)
-        self.encoder = wpilib.Encoder(0,1)
+        # self.encoder = wpilib.Encoder(0,1)
         
         #display motor rpm
         NetworkTables.initialize()
@@ -37,24 +37,24 @@ class Robot(wpilib.TimedRobot):
         print("TELEOP BEGINS")
 
         # Encoder parameters initialization.
-        self.encoder.reset()
-        #self.encoder.setDistancePerPulse(DISTANCE_PER_PULSE)
-        #self.encoder.setMaxPeriod(MAX_PERIOD)
-        #self.encoder.setMinRate(MIN_RATE)
+        # self.encoder.reset()
+        # self.encoder.setDistancePerPulse(DISTANCE_PER_PULSE)
+        # self.encoder.setMaxPeriod(MAX_PERIOD)
+        # self.encoder.setMinRate(MIN_RATE)
 
 
-        #self.motor2 = ctre.WPI_TalonSRX(PORT)
-       # self.drive = DifferentialDrive(self.motor1, self.motor2)
+        # self.motor2 = ctre.WPI_TalonSRX(PORT)
+        # self.drive = DifferentialDrive(self.motor1, self.motor2)
         # setup wheel diameter
 
     def teleopPeriodic(self):
         forward = self.stick.getRawAxis(5)
         self.motor1.set(forward)
-        self.motor2.set(forward)
-        #print(self.encoder.get())
-        QuadPosition = self.motor1.getQuadraturePosition()
-        print(QuadPosition)
-        self.sd.putNumber("ShooterRPM", QuadPosition)
+        self.motor2.set(-forward)
+        # print(self.encoder.get())
+        # QuadPosition = self.motor1.getQuadraturePosition()
+        # print(QuadPosition)
+        # self.sd.putNumber("ShooterRPM", QuadPosition)
 
 
 if __name__ == "__main__":
